ddbe92321b49f55687da29be18330275ae5f69c8
Asphar <davidnguyen7500@gmail.com>
2023-01-12 14:16:25 -0500

    Add dev.sh and dev.md

---
+++ dev.md
@@ -0,0 +1,5 @@
+##Run dev.sh
+
+###Add a microservice
+
+Run a microservice as a folder in a "backend" directory
---
+++ dev.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+echo "Starting..."
+
+sleep 1 
+
+docker run --rm -d --name mongodb --mount source=mongo_data,target=/data -p 27017:27017 mongo:latest
+
+sleep 1
+
+echo "Waiting for the mongodb container to start..."
+
+while [ "$(docker inspect -f '{{.State.Running}}' mongodb)" != "true" ]; do
+    sleep 1
+done
+
+echo "Mongodb container started!"
+
+#for folder in ./backend/*
+
+folders=("/backend/*/" "/client/")
+
+for folder in "${folders[@]}"
+
+do
+  (cd $folder && yarn dev) &
+done
